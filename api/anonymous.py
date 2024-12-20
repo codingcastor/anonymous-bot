95bc58a32c59df02ff22582bd5243294404df573
codingcastor <10059619+codingcastor@users.noreply.github.com>
2024-12-20 14:02:31 +0100

    refacto: Move DB conn to function

--- api/anonymous.py
+++ api/anonymous.py
@@ -1,8 +1,28 @@
 from http.server import BaseHTTPRequestHandler
-import json
 import requests
 from urllib.parse import parse_qs
-from db import store_message
+import os
+import psycopg2
+from datetime import datetime
+
+def get_db_connection():
+    """Get a PostgreSQL database connection"""
+    return psycopg2.connect(os.getenv('DATABASE_URL'))
+
+def store_message(text, user_id, user_name, channel_id, channel_name):
+    """Store a new message in the database"""
+    conn = get_db_connection()
+    cur = conn.cursor()
+    
+    cur.execute('''
+        INSERT INTO messages (text, user_id, user_name, channel_id, channel_name, created_at)
+        VALUES (%s, %s, %s, %s, %s, %s)
+    ''', (text, user_id, user_name, channel_id, channel_name, datetime.now()))
+    
+    conn.commit()
+    cur.close()
+    conn.close()
+
 
 
 class handler(BaseHTTPRequestHandler):
